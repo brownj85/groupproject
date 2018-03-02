9eebec237dc0754e466cf11bbd27c4e85528cea9
Ian Cappellani <ian.cappellani@mail.utoronto.ca>
2018-03-01 19:25:48 -0500

    Implemented the Order class.

---
+++ phase1/src/Order.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Order extends Task implements Observer {
+
+    private boolean prepared = false;
+    private Server server;
+    private Table table;
+    private ArrayList<Dish> dishes = new ArrayList<Dish>();
+
+    public Order(Server server, Table table) {
+        this.server = server;
+        this.table = table;
+    }
+
+    public void addDish(Dish d) {
+        dishes.add(d);
+    }
+
+    public double getPrice() {
+        double price = 0.0;
+        for (Dish dish : dishes) {
+            price += dish.getPrice();
+        }
+        return price;
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        boolean allComplete = true;
+        for (Dish dish : dishes) {
+            if (!dish.isCompleted()) {
+                allComplete = false;
+                break;
+            }
+        }
+        if (allComplete) {
+            prepared = true;
+            setChanged();
+            notifyObservers();
+        }
+    }
+}

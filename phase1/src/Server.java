bf7f3318dcc6dc8e8a8bd6859060ae80f0a46d96
Alex <alexander.rodriguez@mail.utoronto.ca>
2018-03-03 18:19:06 -0500

    Created Class Server.java with basic methods

---
+++ phase1/src/Server.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class Server extends Worker {
+
+    private Order currOrder;
+    private ArrayList<Table> tables;
+
+    public Server(FrontHouse frontHouse, ArrayList<Table> tables){
+        super(frontHouse);
+        this.tables = tables;
+    }
+
+    public void addTable(Table table){
+        this.tables.add(table);
+    }
+
+    public void createOrder(Table table){
+        Order newOrder = new Order(this, table);
+        this.currOrder = newOrder;
+    }
+
+    public void updateOrder(Dish dish){
+        currOrder.addDish(dish);
+    }
+
+    // Still need to add other methods
+
+
+
+
+}
